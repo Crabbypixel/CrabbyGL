95ebb28be4ff343c4b5a410dbf4e54e41aedd701
Crabbyfeet <crabbypixel@gmail.com>
2026-05-21 22:28:14 +0530

    Added TODO comment

--- includes/world/World.h
+++ includes/world/World.h
@@ -131,6 +131,7 @@ private:
 	Frustum m_frustum;
 
 	// World-player variables
+    // TODO: Make this dynamic and make user to control - to be done later
     int m_viewDist = 8;			// Chunk load boundary
     int m_unloadDist = 12;		// Chunk unload boundary
     glm::ivec2 m_lastPlayerChunk = { INT_MAX, INT_MAX };	// Previous frame player chunk pos
