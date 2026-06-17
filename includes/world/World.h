b17ce776514b8639b3279ceb84af5e5731a91a45
Crabbyfeet <crabbypixel@gmail.com>
2026-06-17 17:31:09 +0530

    Final lighting update

--- includes/world/World.h
+++ includes/world/World.h
@@ -150,8 +150,8 @@ private:
 	// World-player variables
     // TODO: Make this dynamic and make user to control - to be done later
     // Chunk load and unload distance
-    int m_viewDist = 4;
-    int m_unloadDist = 4;
+    int m_viewDist = 3;
+    int m_unloadDist = 3;
     glm::ivec2 m_lastPlayerChunk = { INT_MAX, INT_MAX };	// Previous frame player chunk pos
 
 	// Global atomic shutdown flag for workers to exit
